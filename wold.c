618a6f652136e8bd27e1308d5bc50d18a61a3076
Paulius Zaleckas <paulius.zaleckas@gmail.com>
2013-02-13 23:32:26 +0200

    Execute some action on WOL

--- wold.c
+++ wold.c
@@ -4,14 +4,52 @@
 #include <limits.h>
 #include <sys/socket.h>
 #include <netinet/ip.h>
+#include <sys/wait.h>
 #include <string.h>
 #include <errno.h>
 #include <syslog.h>
+
+static char *action = "/etc/wol.action";
 
 static void err_handler(char *err)
 {
 	syslog(LOG_ERR, "%s: %m", err);
 	exit(errno);
+}
+
+static int run_action(void)
+{
+	pid_t pid;
+	int rv;
+	int commpipe[2];		/* This holds the fd for the input & output of the pipe */
+
+	/* Setup communication pipeline first */
+	if (pipe(commpipe))
+		err_handler("Pipe error!");
+
+	/* Attempt to fork and check for errors */
+	pid = fork();
+	if (pid == -1)
+		err_handler("Fork failed");  /* something went wrong */
+
+	if (pid) {
+		/* A positive (non-negative) PID indicates the parent process */
+		dup2(commpipe[1],1);	/* Replace stdout with out side of the pipe */
+		close(commpipe[0]);		/* Close unused side of pipe (in side) */
+		setvbuf(stdout,(char*)NULL,_IONBF,0);	/* Set non-buffered output on stdout */
+		wait(&rv);				/* Wait for child process to end */
+		return rv;
+	} else {
+		/* A zero PID indicates that this is the child process */
+		dup2(commpipe[0], 0);	/* Replace stdin with the in side of the pipe */
+		close(commpipe[1]);		/* Close unused side of pipe (out side) */
+		/* Replace the child fork with a new process */
+		if (execl(action, action, NULL) == -1)
+			err_handler("Failed to run action");
+	}
+
+	/* to silence compiler warning */
+	return 0;
 }
 
 static void listen_wol(uint16_t port)
@@ -49,7 +87,7 @@ static void listen_wol(uint16_t port)
 		}
 
 		syslog(LOG_INFO, "Received WOL packet");
-
+		syslog(LOG_INFO, "%s returned %i", action, run_action());
 	}
 }
 
@@ -66,7 +104,7 @@ int main(int argc, char *argv[])
 	int foreground = 0;
 	unsigned long int port = 9;
 
-	while ((opt = getopt(argc, argv, "p:f")) != -1) {
+	while ((opt = getopt(argc, argv, "p:fa:")) != -1) {
 		switch (opt) {
 			case 'p':
 				port = strtoul(optarg, NULL, 0);
@@ -78,10 +116,18 @@ int main(int argc, char *argv[])
 			case 'f':
 				foreground = 1;
 				break;
+			case 'a':
+				action = strdup(optarg);
+				break;
 			default:
 				usage();
 				exit(EXIT_FAILURE);
 		}
+	}
+
+	if (access(action, X_OK)) {
+		fprintf(stderr, "%s: %s\n", action, strerror(errno));
+		exit(errno);
 	}
 
 	if (!foreground) {
